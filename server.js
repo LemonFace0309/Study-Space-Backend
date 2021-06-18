dde639fbded55104bbd734eb95e516c19d3412af
Jimmy Yang <jimmyy1337@gmail.com>
2021-06-18 00:31:10 -0400

    Sockets for multi vid and chat

--- server.js
+++ server.js
@@ -1,37 +1,57 @@
-const express = require('express')
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
 const cors = require('cors')
-
-const app = express()
-
-app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send('Server is running')
-})
-
-const server = app.listen(process.env.PORT || 8080)
-const io = require('socket.io')(server, {
+const io = socket(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
   },
-})
-io.on('connection', (socket) => {
-  console.log('connection!', socket.id)
-  socket.on('getId', () => {
-    socket.emit('me', socket.id)
-  })
+});
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+  socket.on("join room", roomID => {
+    if (users[roomID]) {
+      const length = users[roomID].length;
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", payload => {
+    io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+  });
+
+  socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+  });
+
   socket.on('disconnect', () => {
-    socket.broadcast.emit('callended')
-  })
-  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit('callUser', {
-      signal: signalData,
-      from,
-      name,
-    })
-  })
-  socket.on('answerCall', ({ to, name, signal}) => {
-    io.to(to).emit('callAccepted', { name, signal })
-  })
-})
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter(id => id !== socket.id);
+      users[roomID] = room;
+    }
+  });
+
+});
+
+server.listen(process.env.PORT || 8080, () => console.log('Server is running on port 8080'));
+
+
