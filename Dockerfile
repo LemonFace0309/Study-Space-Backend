b49f830ee8bbd5479f553312e469ce21f3ece67e
LemonFace0309 <charles.liu0309@gmail.com>
2021-08-07 02:29:23 -0400

    minor changes

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,9 @@ COPY . .
 
 EXPOSE 8080
 
+ENV PORT=8080
+ENV REDIS_PORT=6379
+ENV REDIS_URL=localhost
+
 CMD ["npm", "start"]
 
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.send('Server is running on 8080 🥳');
 });
 
+
 const server = app.listen(process.env.PORT || 8080);
 
 const io = require('socket.io')(server, {
