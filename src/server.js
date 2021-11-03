b5836b4efd711111641453ac952f5bd671cc5b88
Charles Liu <charles.liu0309@gmail.com>
2021-11-03 19:48:59 -0400

    added support for admin communication on the same namespace. Will change to different namespace in the future

--- src/server.js
+++ src/server.js
@@ -28,10 +28,20 @@ const redisClient = {
   rpush: async () => null,
 };
 
+const admins = [];
 const users = {};
 const socketToRoom = {};
 
+/**
+ * Todo: move admin logic to a seperate namespace
+ */
 io.on('connection', (socket) => {
+  socket.on('join admin', async (payload) => {
+    admins.push({ username: payload.username, socketId: socket.id });
+    io.emit('new admin', { username: payload.username, socketId: socket.id });
+    socket.emit('users', { users: Object.values(users).flat() });
+  });
+
   socket.on('join room', async (payload) => {
     socket.join(payload.roomId);
 
@@ -63,6 +73,15 @@ io.on('connection', (socket) => {
         } else {
           socket.emit('other users', { users: usersInThisRoom });
         }
+        socket.emit('admins', { admins });
+        /**
+         * Todo: move to separate namespace later
+         */
+        admins.forEach((admin) => {
+          socket.to(admin.socketId).emit('new user', {
+            user: { socketId: socket.id, username: payload.username, userId: payload.userId, role: payload.role },
+          });
+        });
       })
       .catch((err) => {
         console.warn(err);
@@ -97,6 +116,18 @@ io.on('connection', (socket) => {
     io.to(roomId).emit('message', { message, username });
   });
 
+  // Todo: add redis caching
+  socket.on('dm', ({ message, username, socketId }) => {
+    let recipient = '';
+    try {
+      const roomId = socketToRoom[socketId];
+      recipient = (users[roomId].find((user) => user.socketId === socketId) || {}).username;
+    } catch (err) {
+      console.warn(err);
+    }
+    io.to(socketId).to(socket.id).emit('dm', { message, username, recipient });
+  });
+
   socket.on('isAudioEnabled', ({ enabled }) => {
     const roomId = socketToRoom[socket.id];
     socket.to(roomId).emit('isAudioEnabled', { id: socket.id, enabled });
@@ -114,27 +145,44 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', async () => {
     const roomId = socketToRoom[socket.id];
-    let room = users[roomId];
-    if (room) {
-      // removes user from room
-      room = room.filter((user) => user.socketId !== socket.id);
-      users[roomId] = room;
+    // user is not admin
+    if (roomId) {
+      let room = users[roomId];
+      if (room) {
+        // removes user from room
+        room = room.filter((user) => user.socketId !== socket.id);
+        users[roomId] = room;
+      }
+      try {
+        const filter = { spaceId: roomId };
+        const participants = users[roomId].map((user) => ({ username: user.username, userId: ObjectId(user.userId) }));
+        const update = {
+          $set: { participants },
+        };
+        const client = await dbConnect();
+        await client.db(process.env.DATABASE).collection('spaces').updateOne(filter, update);
+      } catch (err) {
+        console.warn('Unable to remove user from space:', err);
+      }
+      socket.to(roomId).emit('user disconnect', {
+        room,
+        roomId,
+        users: users[roomId],
+      });
+      /**
+       * Todo: move to separate namespace later
+       */
+      const newUsers = Object.values(users).flat();
+      admins.forEach((admin) => {
+        socket.to(admin.socketId).emit('user disconnect', {
+          users: newUsers,
+        });
+      });
+    } else {
+      // user is admin
+      const index = admins.findIndex((admin) => admin.socketId === socket.id);
+      admins.splice(index, 1);
+      io.emit('admin disconnect', { admins });
     }
-    try {
-      const filter = { spaceId: roomId };
-      const participants = users[roomId].map((user) => ({ username: user.username, userId: ObjectId(user.userId) }));
-      const update = {
-        $set: { participants },
-      };
-      const client = await dbConnect();
-      await client.db(process.env.DATABASE).collection('spaces').updateOne(filter, update);
-    } catch (err) {
-      console.warn('Unable to remove user from space:', err);
-    }
-    io.emit('user disconnect', {
-      room,
-      roomId,
-      users: users[roomId],
-    });
   });
 });
